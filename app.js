d70975badf7184bb0c2f422d8a86ae89a86510b8
sboucher <sboucher2011@gmail.com>
2021-11-30 16:42:15 -0500

    capture input

---
+++ app.js
@@ -0,0 +1,10 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+// Notice the lack of parentheses around the `profileDataArr` parameter?
+const printProfileData = profileDataArr => {
+    for (let i = 0; i < profileDataArr.length; i++) {
+      console.log(profileDataArr[i]);
+    }
+  };
+  
+  printProfileData(profileDataArgs);
